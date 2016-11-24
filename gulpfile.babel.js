fa6ffa3569dc1f08915273b122be260806cbcf18
David Sinclair <david@sikhote.com>
2016-11-24 10:02:02 -0800

    gulp done

---
+++ gulpfile.babel.js
@@ -0,0 +1,39 @@
+import gulp from 'gulp';
+import gutil from 'gulp-util';
+import sass from 'gulp-sass';
+import runSequence from 'run-sequence';
+import browserSync from 'browser-sync';
+import autoprefixer from 'autoprefixer';
+import postcss from 'gulp-postcss';
+
+const themePath = './src/wp-content/themes/brandefined-divi/';
+const sassPaths = {
+  blob: `${themePath}**/*.scss`,
+  source: `${themePath}style.scss`
+};
+let bs = browserSync.create()
+
+gulp.task(
+  'sass:compile',
+  () => gulp.src(sassPaths.source)
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(postcss([autoprefixer({browsers: ['last 2 versions'] })]))
+    .pipe(gulp.dest(themePath))
+    .pipe(bs.stream())
+);
+
+gulp.task('sass:watch', () => {gulp.watch(sassPaths.blob, ['sass:compile']);});
+
+gulp.task(
+  'browserSync',
+  () => {
+    bs.init({
+      proxy: 'http://localhost:8080',
+      open: false
+    });
+  }
+);
+
+gulp.task('default', done => {
+  runSequence('sass:compile', 'sass:watch', 'browserSync', done);
+});
--- readme.md
+++ readme.md
@@ -7,6 +7,16 @@ docker-compose up -d
 docker-compose stop
 ```
 
+## compiling scss and browsersync
+```
+npm start
+```
+
+## update npm modules
+```
+npm run update
+```
+
 ## view wordpress 
 ```
 http://localhost:8080
